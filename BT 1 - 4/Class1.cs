7361db62ae1cb6686a40dff8067ea5b8b607f50e
SamNguyen228 <147117476+SamNguyen228@users.noreply.github.com>
2024-05-30 22:55:39 +0700

    Add files via upload

---
+++ BT 1 - 4/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace BT1_to_4
+{
+    class Pr2
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1: Data TranSsformation
+            Console.WriteLine("Exercise 1: Data Transformation");
+            Console.Write("Please enter an integer: ");
+            int userInput1 = int.Parse(Console.ReadLine());
+            double doubleNumber = (double)userInput1;
+            Console.WriteLine($"The number you entered is {userInput1} and as a double is {doubleNumber}.000");
+            Console.WriteLine();
+
+            // Exercise 2: Converting between numeric types
+            Console.WriteLine("Exercise 2: Converting between numeric types");
+            Console.Write("Please enter a real number: ");
+            double userInput2 = double.Parse(Console.ReadLine());
+            int intNumber = (int)userInput2;
+            long longNumber = (long)userInput2;
+            Console.WriteLine($"The number you entered is {userInput2}, as an int is {intNumber} and as a long is {longNumber}");
+            Console.WriteLine();
+
+            // Exercise 3: Convert between char type and int type
+            Console.WriteLine("Exercise 3: Convert between char type and int type");
+            Console.Write("Please enter a character: ");
+            char userInput3 = char.Parse(Console.ReadLine());
+            int asciiCode = (int)userInput3;
+            Console.WriteLine($"The character you entered is '{userInput3}', its ASCII code is {asciiCode}");
+            Console.WriteLine();
+
+            // Exercise 4: Find the maximum value in an array of integers
+            Console.WriteLine("Exercise 4: Find the maximum value in an array of integers");
+            Console.WriteLine("How many integers do you want to enter?");
+            int arraySize = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[arraySize];
+
+            Console.WriteLine($"Please enter {arraySize} integers: ");
+            for (int i = 0; i < arraySize; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxValue = numbers.Max();
+
+            Console.WriteLine($"The maximum value in the array is {maxValue}");
+            Console.ReadLine();
+        }
+    }
+}
