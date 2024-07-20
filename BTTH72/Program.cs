29ce537f73b97951816c39d956d10ee5c00e63cd
SamNguyen228 <147117476+SamNguyen228@users.noreply.github.com>
2024-07-20 22:32:45 +0700

    Add files via upload

---
+++ BTTH72/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+namespace Database_Operation
+{
+    class DBConn
+    {
+        // Main Method
+        static void Main()
+        {
+            Connect();
+            Console.ReadKey();
+        }
+
+        static void Connect()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string constr;
+            SqlConnection conn;
+            constr = @"Data Source=LAPTOP-PPUJ4OAL\SQLEXPRESS;Initial Catalog=Demodb;User ID=sa;Password=cmcuni";
+            conn = new SqlConnection(constr);
+            conn.Open();
+            Console.WriteLine("Mở kết nối!");
+            // Đóng kết nối
+            conn.Close();
+        }
+    }
+}
