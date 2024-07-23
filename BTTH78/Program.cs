ee0d1ea248f4713e3be881f39cee20239d1db3e9
SamNguyen228 <147117476+SamNguyen228@users.noreply.github.com>
2024-07-23 18:04:24 +0700

    Add files via upload

---
+++ BTTH78/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        // Phần sử dụng Thread
+        var thread = new Thread(() =>
+        {
+            Console.WriteLine("Thread bắt đầu.");
+            Thread.Sleep(5000); // Tạm dừng thread trong 1 giây
+            Console.WriteLine("Thread kết thúc.");
+        });
+
+        thread.Start();
+        thread.Join(); // Đợi thread hoàn thành
+
+        // Phần sử dụng async/await
+        await Async1(); // Gọi hàm async
+    }
+
+    public static async Task Async1()
+    {
+        Console.WriteLine("Async bắt đầu.");
+        await Task.Delay(10000); // Tạm dừng không đồng bộ trong 1 giây
+        Console.WriteLine("Async kết thúc.");
+    }
+}
---
+++ BTTH79/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        // Queue a task to the ThreadPool
+        ThreadPool.QueueUserWorkItem(new WaitCallback(DoWork));
+        // Main thread continues to run
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Main thread: " + i);
+            Thread.Sleep(100);
+        }
+        // Give some time for the ThreadPool thread to complete the work
+        Thread.Sleep(1000);
+    }
+    static void DoWork(Object stateInfo)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("ThreadPool thread: " + i);
+            Thread.Sleep(100);
+        }
+    }
+}
